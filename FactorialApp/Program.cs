d3705a478ed32903d49f671b1825fa4de418118b
D. Moschokarfi <despinamx16@gmail.com>
2025-10-02 19:14:33 +0300

    feat: expressionsApp, factorial app and fibo apps (with iterative, recursive and array solutions) created

---
+++ FactorialApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Numerics;
+
+namespace FactorialApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 20; i++)
+            {
+                Console.WriteLine($"Factorial of {i} (iterative) = {FactoIterative(i)}");
+                Console.WriteLine($"Factorial of {i} (recursive) = {FactoRecursive(i)}");
+            }
+        }
+
+        public static BigInteger FactoIterative(int n)
+        {
+            BigInteger result = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+
+        public static BigInteger FactoRecursive(int n)
+        {
+            return (n > 1) ? n * FactoRecursive(n - 1) : 1;
+        }
+    }
+}
