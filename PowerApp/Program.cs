6d2d430300187f51dcfba36e8493ccf996bcff3f
D. Moschokarfi <despinamx16@gmail.com>
2025-10-04 08:33:24 +0300

    feat: new projects added for counting negatives app, calculating power and primes app

---
+++ PowerApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Numerics;
+
+namespace PowerApp
+{
+    /// <summary>
+    ///Calculates b^power, ex. 2^10 = 1024.
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int myBase = 2;
+            int power = 10;
+            BigInteger result = 1;
+
+            for (int i = 1; i <= power; i++)
+            {
+                result *= myBase; // result = result * myBase
+            }
+
+            Console.WriteLine($"{myBase}^{power} = {result}");
+        }
+    }
+}
---
+++ PrimesApp/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace PrimesApp
+{
+    /// <summary>
+    /// Calculates if a number is Prime.
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 20; i++)
+            {
+                if (isPrime(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        public static bool isPrime(int n)
+        {
+            bool prime = true;
+
+            for (int divider = 2; divider <= Math.Sqrt(n); divider++)
+            {
+                if (n % divider == 0)
+                {
+                    prime = false;
+                    break;
+                }
+            }
+            return prime;
+        }
+    }
+}
