043442f688c1a354201b333baca75155019a0484
D. Moschokarfi <despinamx16@gmail.com>
2025-10-08 19:49:37 +0300

    feat: app created that calculates if a triangle is right based on the size of its sides

---
+++ RightTriangleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace RightTriangleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double EPSILON = 0.000005;
+            bool isRight = false;
+
+            Console.WriteLine("Please give the lengths of the three sides of the triangle (a, b, c): ");
+           
+            if (!double.TryParse(Console.ReadLine(), out double a))
+            {
+                Console.WriteLine("Format error. Must be double.");
+            }
+
+            if (!double.TryParse(Console.ReadLine(), out double b))
+            {
+                Console.WriteLine("Format error. Must be double.");
+            }
+
+            if (!double.TryParse(Console.ReadLine(), out double c))
+            {
+                Console.WriteLine("Format error. Must be double.");
+            }
+
+            Console.WriteLine($"The lengths of the three sides of the triangle are: a={a}, b={b}, c={c}");
+
+            isRight = Math.Abs(a * a - b * b - c * c) < EPSILON;
+            Console.WriteLine("The triangle is {0} right", (isRight) ? " " : "not");
+        }
+    }
+}
