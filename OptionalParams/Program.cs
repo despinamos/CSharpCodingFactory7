6a75a48d2005d67ef2073b8c233808a48d922ee6
D. Moschokarfi <despinamx16@gmail.com>
2025-10-11 11:55:50 +0300

    feat: apps that showcase the usage of parameters and optional parameters

---
+++ OptionalParams/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace OptionalParams
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = getDateTime();
+            Console.WriteLine($"{dateTime:D}");
+
+            DateTime dateTime2 = getDateTime();
+            Console.WriteLine($"{dateTime2:F}");
+        }
+
+        public static DateTime getDateTime(int year=2024, int month=11, int day=1, int min=0, int seconds=0, int milli=0)
+        {
+            return new DateTime(year, month, day, min, seconds, milli, DateTimeKind.Utc);
+        }
+    }
+}
---
+++ ParamsApp/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ParamsApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+            result = Add(1, 5, 3, 7);
+            Console.WriteLine(result);
+            result = Add(1, 5);
+            Console.WriteLine(result);
+        }
+
+        /// <summary>
+        /// A type of overloading method that accepts a variable number of arguments.
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns></returns>
+        public static int Add(params int[] numbers)
+        {
+            int sum = 0;
+            foreach (int num in numbers)
+            {
+                sum += num;
+            }
+            return sum;
+        }
+    }
+}
