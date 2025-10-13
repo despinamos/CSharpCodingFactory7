c425ad307df7cefe640d0b4a57efaa5c99ce2985
D. Moschokarfi <despinamx16@gmail.com>
2025-10-13 19:08:22 +0300

    feat: copies examples

---
+++ CopyingApp/Model/Address.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyingApp.Model
+{
+    internal class Address
+    {
+        public string? StreetName { get; set; }
+        public string? StreetNumber { get; set; }
+        public string? ZipCode { get; set; }
+
+        public Address() { }
+
+        public Address(string streetName, string streetNumber, string zipCode)
+        {
+            StreetName = streetName;
+            StreetNumber = streetNumber;
+            ZipCode = zipCode;
+        }
+
+        public override string ToString()
+        {
+            return $"{StreetName} {StreetNumber}, {ZipCode}";
+        }
+    }
+}
---
+++ CopyingApp/Model/Student.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyingApp.Model
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public Address? Address { get; set; }
+
+        public Student() { }
+
+        public Student(int id, string firstName, string lastName, Address address)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}: {FirstName} {LastName}, Address: {Address}";
+        }
+
+        // Returns a reference to the current instance
+        public Student GetRef()
+        {
+            return this;
+        }
+
+        // Returns a shallow copy of the current instance
+        public Student ShallowCopy()
+        {
+            return (Student)MemberwiseClone();
+        }
+
+        // Returns a deep copy of the current instance
+        public Student GetDeepCopy()
+        {
+            Student student = (Student)MemberwiseClone();
+            student.Address = new Address()
+            {
+                StreetName = Address!.StreetName,
+                StreetNumber = Address!.StreetNumber, 
+                ZipCode = Address!.ZipCode
+            };
+            return student;
+        }
+    }
+}
