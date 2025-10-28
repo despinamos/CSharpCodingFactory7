387d342522b0a3af77beb4487bd2767447a69dc8
D. Moschokarfi <despinamx16@gmail.com>
2025-10-28 08:58:05 +0200

    feat: simple web app created using ssms as db

---
+++ WebStarter6DBApp/Program.cs
@@ -0,0 +1,56 @@
+using Serilog;
+using Serilog.Events;
+using WebStarter6DBApp.Configuration;
+
+namespace WebStarter6DBApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+            builder.Services.AddAutoMapper(cfg => cfg.AddProfile<MapperConfig>());
+            builder.Host.UseSerilog((context, config) =>
+            {
+                config.ReadFrom.Configuration(context.Configuration);
+                //config
+                //.MinimumLevel.Debug()
+                //.MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                //.Enrich.FromLogContext()
+                //.WriteTo.Console()
+                //.WriteTo.File(
+                //    "Logs/logs.txt",
+                //    rollingInterval: RollingInterval.Day,
+                //    outputTemplate: "{Timestamp:dd-MM-yyyy HH:mm:ss:fff zzz} {SourceContext} [{Debug}] " +
+                //    "{Message}{NewLine}{Exception}",
+                //    retainedFileCountLimit: null,
+                //    fileSizeLimitBytes: null
+                //    );
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
+}
